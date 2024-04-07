5604b1a9280fb737eecdf6e023f15105933e9004
Sam Huynh <samhwang2112.dev@gmail.com>
2024-04-07 22:45:13 +1000

    chore: reformat files

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts', 'bin/index.ts'],
   outDir: 'dist',
   format: ['cjs', 'esm'],
   dts: true,
+  sourcemap: true,
 });
