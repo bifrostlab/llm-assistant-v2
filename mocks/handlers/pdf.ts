d230a9936174538ad3310a7cf02864c6c68e64cb
Sam Huynh <samhwang2112.dev@gmail.com>
2024-04-08 12:06:06 +1000

    test: setup test mocks for file downloads

---
+++ mocks/handlers/pdf.ts
@@ -0,0 +1,29 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import { http, HttpResponse } from 'msw';
+
+export const handlers = [
+  http.get('https://example.com/a.pdf', () => {
+    const buffer = fs.readFileSync(path.resolve(__dirname, 'dummy.pdf'));
+    return HttpResponse.arrayBuffer(buffer, {
+      headers: {
+        'Content-Type': 'application/pdf;qs=0.001',
+      },
+    });
+  }),
+
+  http.get('https://docs.google.com/uc', ({ request }) => {
+    const url = new URL(request.url);
+    const id = url.searchParams.get('id');
+    if (!id) {
+      return new HttpResponse(null, { status: 400, statusText: 'Bad Request' });
+    }
+
+    const buffer = fs.readFileSync(path.resolve(__dirname, 'dummy.pdf'));
+    return HttpResponse.arrayBuffer(buffer, {
+      headers: {
+        'Content-Type': 'application/octet-stream',
+      },
+    });
+  }),
+];
