8f6c26b05c73a9fd284801cde656988ab27249f7
Sam Huynh <samhwang2112.dev@gmail.com>
2024-04-10 10:56:06 +1000

    chore: rename pdf handlers

--- mocks/server.ts
+++ mocks/server.ts
@@ -1,4 +1,4 @@
 import { setupServer } from 'msw/node';
-import { handlers } from './handlers/pdf';
+import { handlers as pdfHandlers } from './handlers/pdf';
 
-export const server = setupServer(...handlers);
+export const server = setupServer(...pdfHandlers);
